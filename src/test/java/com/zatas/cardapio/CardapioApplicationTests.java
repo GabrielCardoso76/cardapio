51096c784b4c68cba235c6f32fb1fbc25fcf02ac
GabrielCardoso76 <torresgbriel8@gmail.com>
2025-12-10 22:53:20 -0300

    spring boot intial

---
+++ src/test/java/com/zatas/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zatas.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
